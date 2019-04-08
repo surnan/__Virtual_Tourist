74e1c5ffe17a4cb7cbbfd85b9809b8292bfe8e55
Suresh N <forsuresh@yandex.com>
2019-04-08 15:23:31 -0400

    Added NSFetchResultsController to CollectionMapController

--- __Virtual_Tourist/__Virtual_Tourist/Controllers/2/CollectionMapViewsController+NSFetchedResults.swift
+++ __Virtual_Tourist/__Virtual_Tourist/Controllers/2/CollectionMapViewsController+NSFetchedResults.swift
@@ -11,5 +11,19 @@ import MapKit
 import CoreData
 
 extension CollectionMapViewsController {
-    
+    //MARK:- FetchResults + CoreData
+    func setupFetchedResultsController() {
+        let fetchRequest:NSFetchRequest<Photo> = Photo.fetchRequest()
+        let predicate = NSPredicate(format: "pin == %@", currentPin)
+        fetchRequest.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "index", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
 }
--- __Virtual_Tourist/__Virtual_Tourist/Controllers/2/CollectionMapViewsController.swift
+++ __Virtual_Tourist/__Virtual_Tourist/Controllers/2/CollectionMapViewsController.swift
@@ -19,14 +19,52 @@ protocol CollectionMapViewControllerDelegate {
 
 class CollectionMapViewsController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout,NSFetchedResultsControllerDelegate, CollectionMapViewControllerDelegate, MKMapViewDelegate  {
 
-    
+    //MARK:- Injected Variables
     var currentPin: Pin!   //injected from MapController
     var dataController: DataController! //injected from MapController
     
+    //MARK:- Local Variables
+    let reuseIdLoadingCell = "reuseIdLoadingCell"
+    let reuseIDCellLoaded = "reuseIDCellLoaded"
+    let reuseIDCellIsSelected = "reuseIDCellIsSelected"
+    var fetchedResultsController: NSFetchedResultsController<Photo>!
     
+    
+    //MARK:- Lazy Local Variables
+    lazy var customizedLayout: UICollectionViewFlowLayout = {
+        let columnWidth: CGFloat = 10; let rowHeight: CGFloat = 10
+        let screenWidth = view.bounds.width
+        let cellWidth = (screenWidth - 60) / 3
+        let cellWidth2 = (screenWidth - columnWidth * 5) / 3
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = .init(top: columnWidth, left: columnWidth, bottom: columnWidth, right: columnWidth)
+        layout.itemSize = .init(width: cellWidth2, height:   cellWidth2)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = rowHeight
+        layout.minimumInteritemSpacing = columnWidth
+        return layout
+    }()
+    
+    lazy var myCollectionView: UICollectionView = {
+        var collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: customizedLayout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(FinalCollectionLoadingCell.self, forCellWithReuseIdentifier: reuseIdLoadingCell)
+        collectionView.register(FinalCollectionImageCell.self, forCellWithReuseIdentifier: reuseIDCellLoaded)
+        collectionView.register(FinalCollectionSelectedImageCell.self, forCellWithReuseIdentifier: reuseIDCellIsSelected)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.allowsMultipleSelection = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    
+    //MARK:- Code Starts Here
     override func viewDidLoad() {
         view.backgroundColor = UIColor.yellow
         print("CollectionView ... Pin \(currentPin.coordinate)")
+        setupFetchedResultsController()
     }
 }
 
