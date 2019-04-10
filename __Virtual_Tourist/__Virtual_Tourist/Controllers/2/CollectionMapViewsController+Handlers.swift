50646965662a13b4ee0bee9e8a4a9f8a040acb0a
Suresh N <forsuresh@yandex.com>
2019-04-10 13:56:49 -0400

    Refactored code to make logic easier to follow
    1) Deleted lots of unnecessary comments
    
    CollectionMapViewsController+Handlers.emoveSelectedPictures()
    	DELETED --> if loopIndex == (self.photosArrayFetchCount.count - 1)
    		* Replaced this functionality by utilizing:
    			* queue, queue.async, dispatchWorkItem, barrier
    				* Barrier holds code to update UI

--- __Virtual_Tourist/__Virtual_Tourist/Controllers/2/CollectionMapViewsController+Handlers.swift
+++ __Virtual_Tourist/__Virtual_Tourist/Controllers/2/CollectionMapViewsController+Handlers.swift
@@ -9,109 +9,10 @@
 import UIKit
 import CoreData
 
-//func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//    //Using 'currentPin.urlCount' in case returned photos is between 0 and 25
-//    return Int(currentPin.urlCount)
-//}
 
 extension CollectionMapViewsController {
     
     func removeSelectedPicture() {
-        //Need to make CoreData changes to update currentPin.urlCount before changing array
-        
-    
-        let backgroundContext: NSManagedObjectContext! = dataController.backGroundContext
-        let pagesToDelete = Int32(deleteIndexSet.count)
-        let currentPinID = currentPin.objectID
-        
-        self.deleteIndexSet.forEach { (deleteIndex) in //+1
-            guard let photoToRemove = self.photosArrayFetchCount[deleteIndex.item] else {return}
-            let photoToRemoveID = photoToRemove.objectID
-            
-            dataController.backGroundContext.perform {
-                let backgroundPhoto = backgroundContext.object(with: photoToRemoveID) as! Photo
-                backgroundContext.delete(backgroundPhoto)
-                try? backgroundContext.save()
-            }
-        } //-1
-        
-        
-         dataController.backGroundContext.perform {
-            let backgroundPin = backgroundContext.object(with: currentPinID) as! Pin
-            backgroundPin.photoCount = backgroundPin.photoCount - pagesToDelete
-            backgroundPin.urlCount = backgroundPin.urlCount - pagesToDelete
-            try? backgroundContext.save()
-            print("") //stuff is deleted
-        }
-    }
-    
-    
-
-    
-    
-    func removeSelectedPicture2() {
-        //Need to make CoreData changes to update currentPin.urlCount before changing array
-        
-        
-        
-//        let backgroundContext: NSManagedObjectContext! = dataController.backGroundContext
-//        let pagesToDelete = Int32(deleteIndexSet.count)
-//        let currentPinID = currentPin.objectID
-        let context = dataController.viewContext
-        
-        var currentIndex: Int32 = 0
-        
-        context.perform {
-            for (index, photo) in self.photosArrayFetchCount.enumerated(){
-                guard let _photo = photo else {continue}
-                let _indexPath = IndexPath(item: index, section: 0)
-                let _photoID = _photo.objectID
-                
-                let currentPhoto = context.object(with: _photoID) as! Photo
-                
-                if self.deleteIndexSet.contains(_indexPath){
-                    context.delete(currentPhoto)
-                    
-                    if index == self.photosArrayFetchCount.count - 1 {
-                        let _currentPinID = self.currentPin.objectID
-                        let _currentPin = context.object(with: _currentPinID) as! Pin
-//                        _currentPin.urlCount = currentIndex
-//                        _currentPin.photoCount = currentIndex
-                        
-                        _currentPin.urlCount = _currentPin.urlCount - Int32(self.deleteIndexSet.count)
-                        _currentPin.photoCount = _currentPin.photoCount - Int32(self.deleteIndexSet.count)
-                        
-                        try? context.save()
-                        DispatchQueue.main.async {
-                            self.deleteIndexSet.removeAll()
-                            self.loadCollectionArray()
-                            self.myCollectionView.reloadData()
-                        }
-                    }
-                    continue
-                } else {
-                    currentPhoto.index = currentIndex
-                    currentIndex = currentIndex + 1
-                }
-                
-                if index == self.photosArrayFetchCount.count - 1 {
-                    let _currentPinID = self.currentPin.objectID
-                    let _currentPin = context.object(with: _currentPinID) as! Pin
-                    _currentPin.urlCount = currentIndex
-                    _currentPin.photoCount = currentIndex
-                    try? context.save()
-                    DispatchQueue.main.async {
-                        self.deleteIndexSet.removeAll()
-                        self.loadCollectionArray()
-                        self.myCollectionView.reloadData()
-                    } 
-                }
-            }
-        }
-
-    }
-    
-    func removeSelectedPicture3() {
         //Need to make CoreData changes to update currentPin.urlCount before changing array
         
         let context = dataController.viewContext
@@ -121,91 +22,52 @@ extension CollectionMapViewsController {
         context.perform {
             for (loopIndex, photo) in self.photosArrayFetchCount.enumerated(){
                 guard let _photo = photo else {continue}
-                
-                let _indexPath = IndexPath(item: loopIndex, section: 0)
+                let indexPath = IndexPath(item: loopIndex, section: 0)
                 
                 let _photoID = _photo.objectID; let _currentPinID = self.currentPin.objectID
-                let currentPhoto = context.object(with: _photoID) as! Photo
-                let _currentPin = context.object(with: _currentPinID) as! Pin
+                let currentContextPhoto = context.object(with: _photoID) as! Photo
+                let contextPin = context.object(with: _currentPinID) as! Pin
                 
-                if self.deleteIndexSet.contains(_indexPath){
-                    context.delete(currentPhoto)
-                    _currentPin.urlCount = _currentPin.urlCount - 1
-                    _currentPin.photoCount = _currentPin.photoCount - 1
-                    try? context.save()
+                if self.deleteIndexSet.contains(indexPath){
+                    context.delete(currentContextPhoto)
+                    contextPin.urlCount = contextPin.urlCount - 1
+                    contextPin.photoCount = contextPin.photoCount - 1
                 } else {
-                    currentPhoto.index = newIndex
+                    currentContextPhoto.index = newIndex
                     newIndex = newIndex + 1
-                    try? context.save()
                 }
-                
-                if loopIndex == (self.photosArrayFetchCount.count - 1) {
-                    DispatchQueue.main.async {
-                        self.deleteIndexSet.removeAll()
-                        self.loadCollectionArray()
-                        self.myCollectionView.reloadData()
-                    }
-                }
+                try? context.save()
             }
         }
-        
     }
     
     func stuff(){
-        
         let queue = DispatchQueue(label: "com.company.app.queue", attributes: .concurrent)
         queue.async {
-            self.removeSelectedPicture3()
+            self.removeSelectedPicture()
         }
 
         
         let dispatchWorkItem = DispatchWorkItem(qos: .default, flags: .barrier) {
             print("#3 finished")
+            DispatchQueue.main.async {
+                self.deleteIndexSet.removeAll()
+                self.loadCollectionArray()
+                self.myCollectionView.reloadData()
+            }
         }
         
-//        let myQueue = OperationQueue()
-//
-//        let blockOne = BlockOperation {
-//            // self.removeSelectedPicture()
-//            self.removeSelectedPicture2()
-//        }
-//
-//
-//        myQueue.addOperations([blockOne], waitUntilFinished: false)
-        print("") //nothing happens here
+        queue.async(execute: dispatchWorkItem)
     }
 
-    
-    
-    /*
- dataController.backGroundContext.perform {
- let backgroundPhotoToRemove = backgroundContext.object(with: photoToRemoveID) as! Photo
- pagesToDelete = pagesToDelete + 1
- backgroundContext.delete(backgroundPhotoToRemove)
- self.currentPin.photoCount = self.currentPin.photoCount - pagesToDelete
- self.currentPin.urlCount = self.currentPin.urlCount - pagesToDelete
- try? backgroundContext.save()
- DispatchQueue.main.async {
- self.deleteIndexSet.removeAll()
- self.loadCollectionArray()
- }
- }
- */
     
     @objc func handleNewLocationButton(_ sender: UIButton){
         if sender.isSelected {
             print("DELETE")
             stuff()
-//            removeSelectedPicture()
-            
-//            myCollectionView.reloadData()
-            
-
         } else {
             print("-- GET NEW PICTURES --")
-            
             deleteAllPhotosOnPin(currentPin)
-            
             dataController.viewContext.perform { //1
                 print("handle -3")
                 //let pinToAdd = Pin(context: self.dataController.viewContext) //moving above 1 causes pin not drop
